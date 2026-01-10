c8f9bd695c991e51f080cfac3c869b41fb353770
Abhishek Modak <modakabhishek245@gmail.com>
2026-01-10 22:13:36 +0530

    repo linked with github and db connection added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
+mongoose
+    .connect('mongodb+srv://abhishekrameshwarmodak_db_user:54Tr80SiAnKftdXi@assessment.1brbcwm.mongodb.net/?appName=assessment')
+    .then(() => console.log('connected to mongodb'))
+    .catch(error => console.log('error while connecting to db: ', error));
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 });
-
-// db details:
-// username: abhishekrameshwarmodak_db_user
-// password: 54Tr80SiAnKftdXi
-// connection string: mongodb+srv://abhishekrameshwarmodak_db_user:54Tr80SiAnKftdXi@assessment.1brbcwm.mongodb.net/?appName=assessment
